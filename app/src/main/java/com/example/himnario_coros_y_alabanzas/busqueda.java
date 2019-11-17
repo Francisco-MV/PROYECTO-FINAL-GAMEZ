40e6c90f12ad028e72a6ab9ae48fe7e40fed7aa5
YeniferCruz20 <cruzy6557@gmail.com>
2019-11-17 14:19:48 -0600

    cree el metodo de busqueda en la calse busqueda

--- app/src/main/java/com/example/himnario_coros_y_alabanzas/busqueda.java
+++ app/src/main/java/com/example/himnario_coros_y_alabanzas/busqueda.java
@@ -1,10 +1,22 @@
 package com.example.himnario_coros_y_alabanzas;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class busqueda extends AppCompatActivity {
 
@@ -22,11 +34,76 @@ public class busqueda extends AppCompatActivity {
     //List<> productosList;
     ProductsAdapter adapter;
 
+    public void consultarDescripcion(final Context context, final String autor){
+
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Espere por favor, Estamos trabajando en su petición en el servidor");
+        progressDialog.show();
+
+        String url  = Config.urlbuscarhimnario;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @SuppressLint("ResourceType")
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.equals("0")) {
+                            Toast.makeText(context, "No se encontrarón resultados para la búsqueda especificada.", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        }else{
+                            try {
+                                /*
+                                Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+                                */
+                                JSONArray jsonArray = new JSONArray(response);
+
+                                String nombre = jsonArray.getJSONObject(0).getString("nombre");
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_busqueda);
+
+                                datos.setNombre(nombre);
+
+                                Intent intent = new Intent(context, MainActivity.class);
+
+                                intent.putExtra("nombre", nombre);
+
+                                //intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                                context.startActivity(intent);
+
+                                progressDialog.dismiss();
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        progressDialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if(error != null){
+                            Toast.makeText(context, "No se ha podido establecer conexión con el servidor. Verifique su acceso a Internet.", Toast.LENGTH_LONG).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("autor", autor);
+                return map;
+            }
+        };
+
+        MySingleton.getInstance(context).addToRequestQueue(stringRequest);
+
     }
+
+
+
 }
