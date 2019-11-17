1596df20ff0bf34b5d39f998379b8358cb80efef
YeniferCruz20 <cruzy6557@gmail.com>
2019-11-17 14:03:07 -0600

    agregue variables en la clase busqueda da error porque hay instancias de clases que aun no ha subido mi grupo

--- app/src/main/java/com/example/himnario_coros_y_alabanzas/busqueda.java
+++ app/src/main/java/com/example/himnario_coros_y_alabanzas/busqueda.java
@@ -1,10 +1,28 @@
 package com.example.himnario_coros_y_alabanzas;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 public class busqueda extends AppCompatActivity {
+
+    Dto datos = new Dto();
+
+    boolean estadoGuardar = false;
+    boolean estadoEliminar = false;
+
+    private ProgressDialog pd;
+    AlertDialog.Builder dialogo1;
+    AlertDialog.Builder dialogo;
+    ProgressDialog progressDialog;
+
+    //List<Productos> productosList;
+    //List<> productosList;
+    ProductsAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
